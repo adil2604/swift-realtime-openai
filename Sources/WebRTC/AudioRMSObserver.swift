7c05b8bada1b714b4f42d83d002c02f7cd6b7250
Adil Khamidullov <AKhamidullov@beeline.kz>
2025-11-30 18:52:45 +0500

    feat: track callback

--- Sources/WebRTC/AudioRMSObserver.swift
+++ Sources/WebRTC/AudioRMSObserver.swift
@@ -1,23 +1,42 @@
 import Foundation
+import AVFAudio
 @preconcurrency import LiveKitWebRTC
 
 final class AudioRMSObserver: NSObject, LKRTCAudioRenderer, @unchecked Sendable {
 	var callback: ((Float) -> Void)?
 
-	func renderData(_ audioData: UnsafeMutablePointer<Int16>?, numberOfFrames: Int, numberOfChannels: Int) {
-		guard let audioData else { return }
-
-		let totalSamples = numberOfFrames * numberOfChannels
-		let frames = UnsafeBufferPointer(start: audioData, count: totalSamples)
+	func render(pcmBuffer: AVAudioPCMBuffer) {
+		let channelCount = Int(pcmBuffer.format.channelCount)
+		let length = Int(pcmBuffer.frameLength)
+		
+		guard length > 0, channelCount > 0 else { return }
 
 		var sum: Float = 0
-		for frame in frames {
-			let v = Float(frame) / 32768.0
-			sum += v * v
+		var totalSamples = 0
+
+		if let floatData = pcmBuffer.floatChannelData {
+			for channel in 0..<channelCount {
+				let data = floatData[channel]
+				for i in 0..<length {
+					let sample = data[i]
+					sum += sample * sample
+				}
+			}
+			totalSamples = length * channelCount
+		} else if let int16Data = pcmBuffer.int16ChannelData {
+			for channel in 0..<channelCount {
+				let data = int16Data[channel]
+				for i in 0..<length {
+					let sample = Float(data[i]) / 32768.0
+					sum += sample * sample
+				}
+			}
+			totalSamples = length * channelCount
 		}
 
-		let rms = sqrt(sum / Float(totalSamples))
-		callback?(rms)
+		if totalSamples > 0 {
+			let rms = sqrt(sum / Float(totalSamples))
+			callback?(rms)
+		}
 	}
 }
-
